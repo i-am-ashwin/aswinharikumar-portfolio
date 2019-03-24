1c46f0dce3cbf4412067b226aab3fbd4002d7667
Aswin Harikumar <ashwin@minebrat.com>
2019-03-24 13:51:34 +0530

    service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Aswin\'s WebApp';
+  constructor(private meta: Meta) {
+    this.meta.addTags([
+      {name: 'title', content: 'Aswin Harikumar - Front-End Software Engineer'},
+      {name: 'description', content: 'I am a Front-End Software Engineer  based in Bangalore , India.'},
+      {name: 'og:type', content: 'website'},
+      {name: 'og:url', content: 'http://aswinharikumar.me'},
+      {name: 'og:title', content: 'Aswin Harikumar - Front-End Software Engineer'},
+      {name: 'og:description', content: 'I am a Front-End Software Engineer  based in Bangalore , India.'},
+      {name: 'og:image', content: '../assets/portfolio.png'},
+      {name: 'twitter:card', content: 'summary_large_image'},
+      {name: 'twitter:url', content: 'http://aswinharikumar.me'},
+      {name: 'twitter:title', content: 'Aswin Harikumar - Front-End Software Engineer'},
+      {name: 'twitter:description', content: 'I am a Front-End Software Engineer  based in Bangalore , India.'},
+      {name: 'twitter:image', content: '../assets/portfolio.png'}
+    ]);
+  }
 }
