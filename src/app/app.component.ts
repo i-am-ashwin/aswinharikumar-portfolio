a556f1582ef35374412b63515b32c2a2dfe32fd5
Aswin Harikumar <ashwin@minebrat.com>
2019-03-24 18:04:00 +0530

    circleci

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,15 @@ export class AppComponent {
       {name: 'title', content: 'Aswin Harikumar - Front-End Software Engineer'},
       {name: 'description', content: 'I am a Front-End Software Engineer  based in Bangalore , India.'},
       {name: 'og:type', content: 'website'},
-      {name: 'og:url', content: 'http://aswinharikumar.me'},
+      {name: 'og:url', content: 'https://aswinharikumar.me'},
       {name: 'og:title', content: 'Aswin Harikumar - Front-End Software Engineer'},
       {name: 'og:description', content: 'I am a Front-End Software Engineer  based in Bangalore , India.'},
-      {name: 'og:image', content: '../assets/portfolio.png'},
+      {name: 'og:image', content: 'https://www.aswinharikumar.me/assets/images/portfolio.png'},
       {name: 'twitter:card', content: 'summary_large_image'},
-      {name: 'twitter:url', content: 'http://aswinharikumar.me'},
+      {name: 'twitter:url', content: 'https://aswinharikumar.me'},
       {name: 'twitter:title', content: 'Aswin Harikumar - Front-End Software Engineer'},
       {name: 'twitter:description', content: 'I am a Front-End Software Engineer  based in Bangalore , India.'},
-      {name: 'twitter:image', content: '../assets/portfolio.png'}
+      {name: 'twitter:image', content: 'https://www.aswinharikumar.me/assets/images/portfolio.png'}
     ]);
   }
 }
